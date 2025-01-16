ec558bc668bde3387e211dd35ad328eecf39de0c
danielullauri93 <ullauriochoadaniel@gmail.com>
2025-01-16 18:02:53 -0500

    Finish Project with useContext, useReducer

--- Shopping-Cart/src/App.jsx
+++ Shopping-Cart/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { products as initialProducts } from './mocks/products.json';
+import Products from './components/products/Products.jsx';
+import Header from './components/Header.jsx';
+import Footer from './components/footer/Footer.jsx';
+import { IS_DEVELOPMENT } from './config.js';
+import { useFilters } from './hooks/useFilters.js';
+import { CartProvider } from './context/cart.jsx';
+import Cart from './components/cart/Cart.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { filterProducts } = useFilters();
+
+  const filteredProducts = filterProducts(initialProducts);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <CartProvider>
+      <Header />
+      <Cart />
+      <Products products={filteredProducts} />
+      {IS_DEVELOPMENT && <Footer />}
+    </CartProvider>
+  );
 }
 
-export default App
+export default App;
